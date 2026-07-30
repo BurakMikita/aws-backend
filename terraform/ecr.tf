565939aca4518778936db5f0166c8191e54edaa8
mburak <mburak@resdata.it>
2026-07-30 18:53:53 +0200

    Update ECR repository configuration to enable force deletion of images on destroy

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "backend" {
   name                 = "backend"
   image_tag_mutability = "MUTABLE"  # позволяет перезаписывать тег latest
+  force_delete         = true       # позволяет terraform destroy снести репозиторий вместе с образами
  
   image_scanning_configuration {
     scan_on_push = true  # ECR сканирует образы на CVE при каждом push
